5373b99ffed5baee8261cf26cdd34cf26d18aefb
Soheeb Amin <soheeb@gmail.com>
2020-04-08 16:22:00 +0300

    complete dna.py files

---
+++ Assigned Projects/Harvard CS50/DNA/DNA.py
@@ -0,0 +1,78 @@
+#run with: python dna.py databases/large.csv sequences/10.txt
+
+from sys import argv
+import csv
+
+#open csv file and copy data
+csvfile = open(argv[1], newline='')
+raw_data_list = csv.reader(csvfile, delimiter=' ')
+
+# create a list of lists for names and sequences, separated by commas.
+names_list = []
+for element in raw_data_list:
+    names_list.append(element[0].split(","))
+
+
+# remove the STRs and turn them into their own list
+str_list = names_list.pop(0)
+
+
+# throw out the "name" at the start of the STR list.
+str_list.pop(0)
+
+# save sequence into a string
+txtfile = open(argv[2], 'r')
+sequence = txtfile.read()
+
+
+# empty list that will populate to be the number of base matches in the seq per base, in order.
+base_list = []
+
+# grab sequence to test and its lengnth against each sequence that matches the legnth, and populate the base list.
+for STR in str_list:
+    len_str = len(STR)
+
+    # keeps track of what position the current base letter is on
+    base_position = 0
+
+    # counts the # of times bases match
+    base_matches = 0
+
+    # keeps track of the longest sequence of that base
+    lead_counter = 0
+
+    # From each base, check the STR against each set of base letters the same legnth as the STR.
+    for base in sequence:
+        seq_to_check = sequence[base_position:base_position + len_str]
+        # if it matches, add one to match count, and jump the legnth of the STR to see if next bunch matches.
+        if seq_to_check == STR:
+            base_matches += 1
+            base_position += len_str
+            # keep track of the longest string of sucessful STR finds.
+            if base_matches > lead_counter:
+                lead_counter = base_matches
+        # set successful match counter to 0, and increment by a single base.
+        else:
+            base_matches = 0
+            base_position += 1
+    base_list.append(lead_counter)
+
+# check base_count against database of people's base counts for a match. Done as a function to return once match is found.
+def check_list(names_list, base_list):
+    for person_base in names_list:
+        i = 0
+        for items in base_list:
+            if int((person_base[i+1])) != base_list[i]:
+                break
+            i += 1
+            if i == len(base_list):
+                return(person_base[0])
+    return("no match")
+
+
+#call previous function using the names_list and base_list to compare the names against the bases in the given sequences.
+print(check_list(names_list, base_list))
+
+#close open files
+txtfile.close()
+csvfile.close()
--- Assigned Projects/Harvard CS50/DNA/readme.md
+++ Assigned Projects/Harvard CS50/DNA/readme.md
@@ -1,5 +1,46 @@
-# CS50 Assignment: DNA
+# CS50 Assignment: Readability
 
-This is one of the few assignments from Harvard's CS50 with no boiler-plate code, and instead, it is to be written from start to finish with one's own approach. The code itself can be re-purposed for broader pattern-matching, so I thought it worth including here.
+This is one of the few assignments from Harvard's CS50 with no boilerplate code, and it is written from the ground up with my own approach.
+
+If you happen to have the STRs (Short Tandem Repeats: a pattern of ATCG that repeats a certain number of times) of someone's DNA, and a sequence of DNA to check against, this program can actually tell if the DNA is a match of not. It is basically how real-world DNA matching happens.
+
+## Usage
+
+The program command line must contain the two arguments:
+
+1- The location of a CSV database that gives STRS to check, and the names of people and their STRs being checked, along with how often they repeat. See the sample below:
+
+```csv
+
+name,AGATC,AATG,TATC
+Alice,2,8,3
+Bob,4,1,5
+Charlie,3,2,5
+
+```
+
+2- The location of a txt file that has one long string as the sequence being checked against.
+
+```txt
+
+
+```
+
+GGGGGACGGTTTGTCTCACGCCTGTTGGT...
+
+The command line of the program would look as such:
+
+```py
+
+
+$ python dna.py databases/large.csv sequences/5.txt
+
+Output: Lavender
+
+```
+
+I have provided the sample databases CS50 provides, along with 5 strings that have matches discoverable using the "large" database.
+
+## Assignment Details
 
 The actual DNA assignment can be found [here](https://cs50.harvard.edu/x/2020/psets/6/dna "DNA Link").
