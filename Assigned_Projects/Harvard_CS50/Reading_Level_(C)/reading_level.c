619898a14a7eb124cef0bd78aa8ad9a65e58e01d
Soheeb Amin <soheeb@gmail.com>
2020-04-09 09:23:59 +0300

    folders renamed

--- Assigned_Projects/Harvard_CS50/Reading_Level_(C)/reading_level.c
+++ Assigned_Projects/Harvard_CS50/Reading_Level_(C)/reading_level.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
+#include <ctype.h>
 
 //prototypes
 int count_letters(char* provided_string);
@@ -10,7 +11,7 @@ int count_sentences(char* provided_string);
 
 int main(void)
 {
-    //get user input, so as long as there is no newline.
+    //get user input up to 10,000 chars, so as long as there is no newline.
     char text[10000];
     printf("Text: ");
     scanf("%[^\n]s", text);
@@ -26,6 +27,7 @@ int main(void)
     //S is = average number of sentences per 100 words
     float S = (sentences*100)/words;
 
+    //reading level determined by the Coleman-Liau index, explaiened in the ReadMe.
     float reading_level = (0.0588 * L) - (0.296 * S) - 15.8;
 
     //round grade
@@ -33,9 +35,9 @@ int main(void)
 
     //enable these print calls to see the letter, word, and sentence counts.
 
-    //printf("Letters: %d\n", count_letters(text));
-    //printf("Words: %d\n", count_words(text));
-    //printf("Sentences: %d\n", count_sentences(text));
+    printf("Letters: %d\n", count_letters(text));
+    printf("Words: %d\n", count_words(text));
+    printf("Sentences: %d\n", count_sentences(text));
 
 
     //deal with less than one and over 16
@@ -51,17 +53,15 @@ int main(void)
         return 0;
     }
 
-    //pring reading level if not
+    //pring reading level if within level range.
     if (reading_level < 15)
     {
         printf("Grade %.0f\n", reading_level);
         return 0;
     }
-
-    //enable these to see the letter, word, and sentence counts.
-
 }
 
+//function to count letters
 int count_letters(char* provided_string)
 {
     //number of letters in string
@@ -70,7 +70,7 @@ int count_letters(char* provided_string)
     //counts letters in string
     for (int i = 0; i < strlen(provided_string); i++)
     {
-        if ((provided_string[i]>='a' && provided_string[i]<='z') || (provided_string[i]>='A' && provided_string[i]<='Z'))
+        if (isalpha(provided_string[i]) != 0)
         {
             letters++;
         }
@@ -78,18 +78,19 @@ int count_letters(char* provided_string)
     return letters;
 }
 
+//function to count words
 int count_words(char* provided_string)
 {
     // word count
     int words = 0;
 
-    //iterate over letters and demarcates a word as a letter followed by a space.
+    //iterate over letters and demarcates a word as space followed by a letter.
     for (int i = 0; i < strlen(provided_string); i++)
     {
+        char character;
         if (provided_string[i] ==' ')
         {
-            if ((provided_string[i+1]>='a' && provided_string[i+1]<='z') || (provided_string[i+1]>='A'
-                 && provided_string[i+1]<='Z'))
+            if(isalpha(provided_string[i+1]) != 0)
             {
                 words++;
             }
@@ -100,23 +101,25 @@ int count_words(char* provided_string)
     return words;
 }
 
-
+//function to count sentences
 int count_sentences(char* provided_string)
 {
     //sentence count
     int sentences = 0;
-
+    char character = 0;
     //iterate over letters and count . ? ! as end of sentence
     for (int i = 0; i < strlen(provided_string); i++)
-        if (provided_string[i] == '.' || provided_string[i] == '?' || provided_string[i] == '!')
+    {
+        character = provided_string[i];
+        if (character == '.' || character == '?' || character == '!')
         {
           sentences++;
-        }
-   //handles the case in which the user provided no punctuation.
+        }   
+    } 
+   //handles the case in which the user provided no punctuation by assuming one sentence.
     if (sentences == 0)
     {
         sentences++;
     }
     return sentences;
 }
-
